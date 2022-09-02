6a538ff2622f3f072e42044cb57448eee8aa98f0
Dennis Schwartz <dschwartz@techfak.uni-bielefeld.de>
2022-09-03 00:13:35 +0200

    enemy does shooti shooti @2hours

--- Assets/EnemyAi.cs
+++ Assets/EnemyAi.cs
@@ -10,31 +10,25 @@ public class EnemyAi : MonoBehaviour
     [Range(0,3)]public int typeOfEnemy;
     public float[] shootCooldown;
     public GameObject[] bullets;
-    public float[] moveSpeed;
-    public float[] moveTime;
-    public float direction;
-    [Header("movin")] public float minDistance;
-    public float maxDistance;
-    public float nextTarget;
-    public float distanceToNext;                //Distance to next target unitl a new one is picked
+//Distance to next target unitl a new one is picked
     [Header("Status")] public bool canShoot;
     void Start()
     {
-        MoveRandomDirection();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (typeOfEnemy == 0) Enemy1();
 
+        if (typeOfEnemy == 0) Enemy1();
+        
     }
 
     private void Enemy1()
     {
         //Shooti
-        // Shoot();
-        //Walki
+        Shoot();
     }
 
     private void Shoot()
@@ -43,7 +37,7 @@ public class EnemyAi : MonoBehaviour
         canShoot = false;
         StartCoroutine(ShootCooldown());
 
-        GameObject go = Instantiate(bullets[typeOfEnemy]);
+        GameObject go = Instantiate(bullets[typeOfEnemy],transform.position,Quaternion.identity);
         BulletScript bs = go.GetComponent<BulletScript>();
         bs.target = Movement.Position;
     }
@@ -54,32 +48,5 @@ public class EnemyAi : MonoBehaviour
         canShoot = true;
     }
 
-    private void MoveRandomDirection()
-    {
-        direction = Random.Range(-1, 1);
-
-
-        StartCoroutine(MOve());
-        //Pick new target between  x 0 and x max
-    }
-
-
-    private IEnumerator MOve()
-    {
-        while (true)
-        {
-            var temp = transform.position;
-            temp.x += moveSpeed[typeOfEnemy] * direction;
-            Debug.Log(moveSpeed[typeOfEnemy]);
-            transform.position = temp;
-        }
-        yield return new WaitForSeconds(moveTime[typeOfEnemy]);
-        StartCoroutine(WaitABit());
-    }
-
-    private IEnumerator WaitABit()
-    {
-        yield return new WaitForSeconds(Random.Range(0.5f, 2));
-        MoveRandomDirection();
-    }
+    
 }
