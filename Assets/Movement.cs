db3e812af3137cb1acaf292df4bc238b63ba872d
Dennis Schwartz <dschwartz@techfak.uni-bielefeld.de>
2022-09-02 17:01:59 +0200

    Temp

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,32 +5,43 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    [Header("Moving")] 
-    [SerializeField] private float force;
-    private Vector3 horizontal;
+    [Header("Moving")] [SerializeField] private float force;
+    private Vector2 horizontal;
+    public Vector3 desiredMove;
     private Vector2 vertical;
-    [Header("CharacterStatus")] public bool grounded;
-    
-    [Header("GroundCheck")]
-    [SerializeField] private LayerMask groundLayer;
+
+    [Header("CharacterStatus")] public bool jumping;
+    public bool grounded;
+
+    [Header("Jumping")] [SerializeField] private float gravityForce;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpDuration;
+    private double timeJumped;
+
+    [Header("GroundCheck")] [SerializeField]
+    private LayerMask groundLayer;
+
     [SerializeField] private float rayCastDistance;
 
-    [Header("Refs")] 
-    private Vector2 position;
-    
+    [Header("Refs")] private Vector2 position;
+    public int test = 0;
+
     // Start is called before the first frame update
     void Start()
     {
+        desiredMove = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Remove later!
-        Debug.DrawRay(transform.position,Vector2.down,Color.green);
-        //
-        
-        if(!grounded)GroundCheck();
+        GroundCheck();
+        JumpDurationCheck();
+        if (!grounded)
+        {
+            Gravity();
+        }
+
         Move();
     }
 
@@ -38,7 +49,9 @@ public class Movement : MonoBehaviour
     public void Horizontal(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>();
+        Debug.Log(horizontal);
     }
+
     //Get vertical Input
     public void Vertical(InputAction.CallbackContext context)
     {
@@ -46,19 +59,77 @@ public class Movement : MonoBehaviour
     }
 
 
-    
     //Checks if player is grounded
     private void GroundCheck()
     {
+        //Remove later!
+        Debug.DrawRay(transform.position, Vector2.down * rayCastDistance, Color.green);
+        //
         RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector2.down, rayCastDistance, groundLayer);
-        if (ray.collider != null) grounded = true;
+        if (ray.collider != null) TouchGround();
         else grounded = false;
+    }
+
+    //Gets called when touching the ground
+    private void TouchGround()
+    {
+        if (grounded) return;
+        grounded = true;
+        desiredMove.y = 0;
     }
 
     private void Move()
     {
-        Vector3 move = horizontal;
-        
-        transform.position += move * force;
+        desiredMove.x = horizontal.x * Time.deltaTime;
+
+
+        transform.position += desiredMove * force;
+    }
+
+    private void Gravity()
+    {
+        if (jumping) return;
+        desiredMove.y = gravityForce;
+    }
+
+    //What Happens when the player triggers the jump
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            if (!CanJump()) return; //Coyote Time
+            //InputBuffer
+            jumping = true;
+            timeJumped = Time.time;
+            desiredMove.y = jumpForce;
+        }
+        if(context.canceled)
+        {
+            test = 0;
+            EndOfJump();
+        }
+    }
+
+    private void JumpDurationCheck()
+    {
+        if (jumping && timeJumped + jumpDuration - Time.time <= 0)
+        {
+            EndOfJump();
+        }
+    }
+
+    //Checks if the player is allowed to jump. Including coyote time
+    private bool CanJump()
+    {
+        if (grounded || jumping) return true;
+        return false;
+    }
+
+
+    //What happens after the jump
+    private void EndOfJump()
+    {
+        Debug.Log(transform.position.y);
+        jumping = false;
     }
 }
