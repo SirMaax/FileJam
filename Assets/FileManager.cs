d1688c186492776bb757bcfe37e241abc97e15b4
Dennis <dennis.schwartz.bielefeld@gmail.com>
2022-09-04 01:44:10 +0200

    Fixed multiple selection stuff

--- Assets/FileManager.cs
+++ Assets/FileManager.cs
@@ -5,12 +5,8 @@ using UnityEngine;
 public class FileManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject[] colliders;      //Here is position of the 3 target boxes
-    public Word[] filePostions;         //Important for later which box is used first
-
-
-
-
+    public GameObject[] colliders; //Here is position of the 3 target boxes
+    public Word[] filePostions; //Important for later which box is used first
 
 
     public void Released(Word word)
@@ -20,17 +16,38 @@ public class FileManager : MonoBehaviour
         {
             if (ele.GetComponent<BoxCollider2D>().bounds.Contains(InputManager.MousePosition))
             {
+                for (int i = 0; i < filePostions.Length; i++)
+                {
+                    if (filePostions[i] == null) continue;
+                    if (filePostions[i].Equals(word))
+                    {
+                        filePostions[i] = null;
+                    }
+                }
+                if (filePostions[ele.GetComponent<TargetWord2>().position] != null)
+                {
+                    filePostions[ele.GetComponent<TargetWord2>().position].GetComponent<Word>().ResetPos();
+                }
                 filePostions[ele.GetComponent<TargetWord2>().position] = word;
                 var temp = ele.transform.position;
                 temp.z -= 1;
                 word.gameObject.transform.position = temp;
                 return;
             }
+                
+
             
         }
+
+        for (int i = 0; i < filePostions.Length; i++)
+        {
+            if (filePostions[i] == null) continue;
+            if (filePostions[i].Equals(word))
+            {
+                filePostions[i] = null;
+            }
+        }
+       
         word.ResetPos();
     }
-    
-    
-    
 }
