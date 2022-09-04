bb26e1d1c9754e24c2de3569a0410236a0404853
Dennis <dennis.schwartz.bielefeld@gmail.com>
2022-09-04 17:50:44 +0200

    enemyspawning yes

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    public Transform[] spawningPoints;
+    public float[] changeOfSpawning;
+
+    [Header("Wave")] public int currentWave;
+    [SerializeField] private float timeBetweenRounds;
+    [Header("Enemy")] public GameObject[] enemy;
+    private bool once = false;
+    /// <summary>
+    /// ////////////////
+    /// </summary>
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnNormal();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckHowManyEnemies();
+    }
+
+    private void SpawnNormal()
+    {
+        foreach (var pos in spawningPoints)
+        {
+            if (Random.value < changeOfSpawning[currentWave])
+            {
+                int enemyType = Random.Range(0, 4);
+                GameObject go = Instantiate(enemy[enemyType], pos.position, Quaternion.identity);
+                go.GetComponent<EnemyAi>().typeOfEnemy = enemyType;
+                
+            }
+        }
+    }
+
+    private void CheckHowManyEnemies()
+    {
+
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if (enemies.Length == 0 && !once)
+        {
+            once = true;
+            StartCoroutine(NextRound());
+        }
+
+    }
+
+    private IEnumerator NextRound()
+    {
+        yield return new WaitForSeconds(timeBetweenRounds);
+        currentWave++;
+        SpawnNormal();
+        once = false;
     }
 }
